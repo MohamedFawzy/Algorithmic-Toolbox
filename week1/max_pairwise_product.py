cd60d145aef9664efacb0df894649639b92200d0
mohamedfawzy <mfawzy22@gmail.com>
2018-05-04 23:30:36 +0200

    updates

--- week1/max_pairwise_product.py
+++ week1/max_pairwise_product.py
@@ -2,7 +2,6 @@
 
 # find max pairwise product
 def max_pairwise_product_naive(n, a):
-
     result = 0
 
     for i in range(0, n):
@@ -12,35 +11,33 @@ def max_pairwise_product_naive(n, a):
 
     return result
 
+
 # optimized version of max pairwise product
 # instated of iterate through two indices which is O(n^2) get first one then next one alone
 
-def max_pairwise_product(n , a):
+def max_pairwise_product(n, a):
+    max_index1 = 0
 
-    index1 = 1
+    for i in range(n):
+        if a[i] > a[max_index1]:
+            max_index1 = i
 
-    for i in range(0, n):
-        if a[i] > a[index1]:
-            index1 = i
-
-    if index1 == 1:
-        index2 = 2
+    if max_index1 == 0:
+        max_index2 = 1
     else:
-        index2 = 2
+        max_index2 = 0
 
-    for j in range(1, n):
-        if a[j] != a[index1] and a[j] != a[index2]:
-            index2 = j
+    for j in range(n):
+        if a[j] != a[max_index1] and a[j] > a[max_index2]:
+            max_index2 = j
 
-    return a[index1] * a[index2]
+    return a[max_index1] * a[max_index2]
 
 
 if __name__ == '__main__':
     n = int(input())
     a = [int(x) for x in raw_input().split()]
-    assert(len(a) == n)
-    print('naive', max_pairwise_product_naive(n, a))
-    print('fast', max_pairwise_product(n, a))
-
-
-
+    assert (len(a) == n)
+    #print('naive', max_pairwise_product_naive(n, a))
+    #print('fast', max_pairwise_product(n, a))
+    print(max_pairwise_product(n,a))
--- week1/max_pairwise_stress_testing.py
+++ week1/max_pairwise_stress_testing.py
@@ -1,29 +1,28 @@
 import random
-from max_pairwise_product_naive import max_pairwise_product_naive
+from max_pairwise_product import max_pairwise_product_naive
 from max_pairwise_product import max_pairwise_product
 
 
-
-
-N = 10
-M = 100000
-
-
-
-def stressTesting(N, M):
+def stress_testing(N, M):
     print "Hello"
     while True:
-        n =  random.randint(2, N)
+        n = random.randint(2, N)
         a = a[1:n]
-        for i in range(1, n):
+        for i in range(0, n):
             a[i] = random.randint(0, M)
         print(a[i:n])
-        result1 = max_pairwise_product_naive(a)
-        result2 = max_pairwise_product(a)
+        result1 = max_pairwise_product_naive(N , a)
+        result2 = max_pairwise_product(N , a)
         if result1 == result2:
             print "OK"
         else:
             print ("Wrong answer: ", result1, result2)
             return
 
-stressTesting(N, M)
+
+if __name__ == '__main__':
+    N = raw_input('enter N : ')
+    M = raw_input('enter M')
+    print(stress_testing(N, M))
+
+
