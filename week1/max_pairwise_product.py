fac9ab1bed65df1645fed12bc89d58017f9ef812
mohamedfawzy <mfawzy22@gmail.com>
2018-05-04 04:28:31 +0200

    add optimized solution version

--- week1/max_pairwise_product.py
+++ week1/max_pairwise_product.py
@@ -1,16 +1,28 @@
 # Uses python2
+# optimized version of max pairwise product
+# instated of iterate through two indices which is O(n^2) get first one then next one alone
 
 n = int(input())
 a = [int(x) for x in raw_input().split()]
 
 assert(len(a) == n)
 
-product = 0
+index1 = 1
 
-for i in range(0, n):
-    for j in range(i+1, n):
-        product = max(product, a[i] * a[j])
+for i in range(2, n):
+    if a[i] > a[index1]:
+        index1 = i
 
-print(product)
+if index1 == 1:
+    index2 = 2
+else:
+    index2 = 2
+
+for j in range(1, n):
+    if a[j] != a[index1] and a[j] != a[index2]:
+        index2 = j
+
+print a[index1] * a[index2]
+
 
 
