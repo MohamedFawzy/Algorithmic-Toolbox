0a50e6aac5dc4156a7f9c3b9720f55962439f0c3
mohamedfawzy <mfawzy22@gmail.com>
2018-05-05 00:55:56 +0200

    add stress testing for algorithm

--- week1/max_pairwise_product.py
+++ week1/max_pairwise_product.py
@@ -1,4 +1,6 @@
 # Uses python2
+import random
+
 
 # find max pairwise product
 def max_pairwise_product_naive(n, a):
@@ -25,14 +27,32 @@ def max_pairwise_product(n, a):
     max_index2 = -1
 
     for j in range(n):
-        if a[j] != a[max_index1] and ( max_index2 == -1 or a[j] > a[max_index2]):
+        if a[j] != a[max_index1] and (max_index2 == -1 or a[j] > a[max_index2]):
             max_index2 = j
 
     return a[max_index1] * a[max_index2]
 
 
 if __name__ == '__main__':
+    ##### stress test as validation model for our algorithm ######
+    while (True):
+        n = random.randrange(2, 10)
+        print "generated random numbers ", n
+        a = []
+        for i in range(n):
+            a.append(i)
+
+        print "generated dictionary ", a
+        result1 = max_pairwise_product_naive(n, a)
+        result2 = max_pairwise_product(n, a)
+        if result1 != result2:
+            print "Wrong Answer Result ", result1, "Result2 ", result2
+            break
+        else:
+            print  "OK"
+    #### end stress testing #####
+
     n = int(input())
     a = [int(x) for x in raw_input().split()]
-    assert(len(a) == n)
+    assert (len(a) == n)
     print(max_pairwise_product(n, a))
