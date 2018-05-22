b421e5fba4f21547884e9e79e87263ef20f55862
Mohamed Fawzy <mfawzy22@gmail.com>
2018-05-22 12:43:27 +0200

    update fibonacci with corner case simplified

--- week2/fibonacci.py
+++ week2/fibonacci.py
@@ -1,15 +1,13 @@
 # Uses python2
 
 def calc_fib(n):
-    if (n == 0):
-        return 0
-    elif (n == 1):
-        return 1
-    else:
-        return calc_fib(n - 1) + calc_fib(n - 2)
+    if (n <= 1):
+        return n
+
+    return calc_fib(n - 1) + calc_fib(n - 2)
 
 
 if __name__ == '__main__':
     n = int(input())
     result = calc_fib(n)
-    print("Result for ", n, " is ", result)
+    print("Result for ", range(n-1), " is ", result)
