21d99633a70c212e9b540504f9ea62f2d55c77bc
Mohamed Fawzy <mfawzy22@gmail.com>
2018-05-25 15:44:02 +0200

    fibonacci numebrs

--- week2/fibonacci.py
+++ week2/fibonacci.py
@@ -1,13 +1,9 @@
-# Uses python2
-
+# Uses python3
 def calc_fib(n):
     if (n <= 1):
         return n
 
     return calc_fib(n - 1) + calc_fib(n - 2)
 
-
-if __name__ == '__main__':
-    n = int(input())
-    result = calc_fib(n)
-    print("Result for ", range(n-1), " is ", result)
+n = int(input())
+print(calc_fib(n))
