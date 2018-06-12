6d76aeb6134375c6d224bc63dc52e2a0c4c48272
Mohamed Fawzy <mfawzy22@gmail.com>
2018-06-12 17:30:08 +0200

    solution for lcm

--- week2/lcm.py
+++ week2/lcm.py
@@ -1,6 +1,5 @@
-# Uses python3
-import sys
-
+# Uses python2
+# LCM(a,b) * GCD(a,b)
 def lcm_naive(a, b):
     for l in range(1, a*b + 1):
         if l % a == 0 and l % b == 0:
@@ -8,8 +7,21 @@ def lcm_naive(a, b):
 
     return a*b
 
+
+def gcd(a, b):
+    if b == 0:
+        return a
+
+    a_reminder = a % b
+
+    return gcd(b, a_reminder)
+
+def lcm(a, b):
+    return (a*b) // gcd(a,b)
+
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
+    input = raw_input()
     a, b = map(int, input.split())
-    print(lcm_naive(a, b))
+    print(lcm(a, b))
 
