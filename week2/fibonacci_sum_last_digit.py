54939d5601d13b5a36b5042e2919a44c983d6edf
Mohamed Fawzy <mfawzy22@gmail.com>
2018-06-21 14:14:05 +0200

    fix result set and sum for accumluate result as sum

--- week2/fibonacci_sum_last_digit.py
+++ week2/fibonacci_sum_last_digit.py
@@ -1,7 +1,23 @@
 # Uses python2
 import sys
 
+
 def fibonacci_sum_naive(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current = 1
+    sum = 1
+
+    for _ in range(n - 1):
+        previous, current = current, previous + current
+        sum += current
+
+    return sum % 10
+
+
+def get_fibonacci(n):
     if n <= 1:
         return n
 
@@ -13,29 +29,18 @@ def fibonacci_sum_naive(n):
         previous, current = current, previous + current
         sum += current
 
-    return sum % 10
-
-def get_fibonacci(n):
-    if n <=1:
-        return n
-
-    previous = 0
-    current = 1
-
-    for _ in range(n -1):
-        previous, current = current, previous+ current
-
-    return current
+    return sum
 
 
 def get_reminder(m):
     previous = 0
     current = 1
 
-    for i in range(m * m +1):
+    for i in range(m * m + 1):
         previous, current = current, (previous + current) % m
-        if previous==0 and current==1:
-            return  i+1
+        if previous == 0 and current == 1:
+            return i + 1
+
 
 def get_fibonacci_huge(n):
     reminder = n % get_reminder(10)
@@ -47,4 +52,4 @@ if __name__ == '__main__':
     input = raw_input()
     n = int(input)
     print(fibonacci_sum_naive(n))
-    print(get_fibonacci(n))
+    print(get_fibonacci_huge(n))
