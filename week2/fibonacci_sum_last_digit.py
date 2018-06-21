419d2c116dacf0708ce83f205eb22a2c94702838
Mohamed Fawzy <mfawzy22@gmail.com>
2018-06-21 14:09:34 +0200

    init quiz 6

--- week2/fibonacci_sum_last_digit.py
+++ week2/fibonacci_sum_last_digit.py
@@ -1,4 +1,4 @@
-# Uses python3
+# Uses python2
 import sys
 
 def fibonacci_sum_naive(n):
@@ -15,7 +15,36 @@ def fibonacci_sum_naive(n):
 
     return sum % 10
 
+def get_fibonacci(n):
+    if n <=1:
+        return n
+
+    previous = 0
+    current = 1
+
+    for _ in range(n -1):
+        previous, current = current, previous+ current
+
+    return current
+
+
+def get_reminder(m):
+    previous = 0
+    current = 1
+
+    for i in range(m * m +1):
+        previous, current = current, (previous + current) % m
+        if previous==0 and current==1:
+            return  i+1
+
+def get_fibonacci_huge(n):
+    reminder = n % get_reminder(10)
+    result = get_fibonacci(reminder)
+    return result
+
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
+    input = raw_input()
     n = int(input)
     print(fibonacci_sum_naive(n))
+    print(get_fibonacci(n))
