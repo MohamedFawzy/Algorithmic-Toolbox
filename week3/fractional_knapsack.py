378a8a537b1ddfa5444b68b4ee76f493afb0420d
Mohamed Fawzy <mfawzy22@gmail.com>
2018-08-09 01:31:23 +0200

    fix test case

--- week3/fractional_knapsack.py
+++ week3/fractional_knapsack.py
@@ -1,31 +1,28 @@
 # Uses python2
 import sys
+import unittest
+
 
 def get_optimal_value(capacity, weights, values):
     value = 0.
-    # write your code here
     # map values and weights then divide them to get the portion per unit.
-    portion = [float(v) / float(w) for v, w in zip(values, weights)]
-    for _ in xrange(len(weights)+1):
+    proportion = [float(v) / float(w) for v, w in zip(values, weights)]
+    for _ in range(len(weights) + 1):
         if capacity == 0:
             return value
             break
-
-        max_w = max(portion)
-        index = portion.index(max_w)
-        portion[index] = -1
+        max_weight = max(proportion)
+        index = proportion.index(max_weight)
+        proportion[index] = -1
         add_capacity = min(capacity, weights[index])
-        value += add_capacity * max_w
+        value += add_capacity * max_weight
         weights[index] -= add_capacity
         capacity -= add_capacity
-
-
     return value
 
 
-
 if __name__ == "__main__":
-    data = list(map(int, raw_input().split()))
+    data = list(map(int, sys.stdin.read().split()))
     n, capacity = data[0:2]
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
