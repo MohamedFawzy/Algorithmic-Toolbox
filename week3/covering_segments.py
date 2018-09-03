180bac78df3916704c36c2a0366507acc03683ac
Mohamed Fawzy <mfawzy22@gmail.com>
2018-09-03 12:52:57 +0200

    fix cases

--- week3/covering_segments.py
+++ week3/covering_segments.py
@@ -1,22 +1,31 @@
-# Uses python3
-import sys
+# Uses python2
 from collections import namedtuple
+from operator import attrgetter
 
 Segment = namedtuple('Segment', 'start end')
 
 def optimal_points(segments):
     points = []
-    #write your code here
-    for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+    segments = sorted(segments, key=attrgetter('end'))
+    max_right = segments[0].end
+    points.append(max_right)
+    i = 1
+    while i < len(segments):
+        if max_right < segments[i].start:
+            max_right = segments[i].end
+            points.append(max_right)
+        i += 1
+
     return points
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, data = list(map(int, input.split()))
+    n = int(raw_input())
+    data = [0] * (2*n)
+    for i in range(n):
+        data[2*i], data[2*i+1] = map(int, raw_input().split())
     segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
     points = optimal_points(segments)
     print(len(points))
     for p in points:
-        print(p)
+        print str(p) + " ",
+
