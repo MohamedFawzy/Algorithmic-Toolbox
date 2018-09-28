cbc41bf0a9ab461b82051c2d8acf27ba8e1f00ec
Mohamed Fawzy <mfawzy22@gmail.com>
2018-09-28 15:50:20 +0200

    fix testcase

--- week4/majority_element.py
+++ week4/majority_element.py
@@ -3,16 +3,11 @@ import sys
 
 
 def get_majority_element(a, left, right):
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
-    # write your code here
     maj_index = 0
     count = 1
+    for i in range(1, right):
 
-    for i in range(1,right):
-        if a[maj_index] == a[i]:
+        if a[i] == a[maj_index]:
             count += 1
         else:
             count -= 1
@@ -21,6 +16,7 @@ def get_majority_element(a, left, right):
             maj_index = i
             count = 1
 
+    count = 0
     for i in range(right):
         if a[i] == a[maj_index]:
             count += 1
@@ -29,10 +25,11 @@ def get_majority_element(a, left, right):
         return a[maj_index]
     return -1
 
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
-    n = data[0] - 1
+    n = data[0]
     a = data[1:]
     if get_majority_element(a, 0, n) != -1:
         print(1)
