735a83edd32418a178edeceb313104c768979090
Mohamed Fawzy <mfawzy22@gmail.com>
2018-10-13 01:38:23 +0200

    add solution

--- week6/knapsack.py
+++ week6/knapsack.py
@@ -1,15 +1,26 @@
 # Uses python2
 import sys
 
-def optimal_weight(W, w):
+import numpy as np
+
+def optimal_weight(W, w,n):
     # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+
+    matrix = np.zeros(shape=(len(w), W + 1), dtype=int)
+
+
+    for j in range(0, len(w)):
+        for i in range(1, W + 1):
+            if w[j] > i:
+                matrix[j, i] = matrix[j - 1, i]
+            else:
+                matrix[j, i] = max(w[j] + matrix[j - 1, i - w[j]], matrix[j - 1, i])
+    return matrix[len(w) - 1, W]
+
+    pass
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    W, n, *w = list(map(int, input.split()))
-    print(optimal_weight(W, w))
+    W, n = list(map(int, raw_input().split()))
+    w = list(map(int, raw_input().split()))
+    print(optimal_weight(W, w,n))
