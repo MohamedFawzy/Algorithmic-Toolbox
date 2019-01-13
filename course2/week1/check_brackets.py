571b5624d75b43915c283561f1379b87ce1e671c
Mohamed Fawzy <mfawzy22@gmail.com>
2019-01-13 14:04:29 +0200

    add week1

--- course2/week1/check_brackets.py
+++ course2/week1/check_brackets.py
@@ -11,20 +11,30 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
+    for i, next in enumerate(text,start=1):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next, i))
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if len(opening_brackets_stack) == 0: return i
+            top = opening_brackets_stack.pop()
+            match = are_matching(top.char, next)
+            if match == False:
+                return i
+
+    return len(opening_brackets_stack)
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    if mismatch == 0:
+        print("Success")
+    else:
+        print(mismatch)
 
 
 if __name__ == "__main__":
